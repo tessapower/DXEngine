fea7fd7cfb965bb069b81da8060728aaa1c900fa
Tessa Power <hello@tessapower.xyz>
2024-11-11 15:37:56 -0600

    Rename MessageMap to message_map

---
+++ DXengine/include/message_map.h
@@ -0,0 +1,17 @@
+#ifndef MESSAGE_MAP_H
+#define MESSAGE_MAP_H
+
+#include "stdafx.h"
+#include <unordered_map>
+#include <string>
+
+class message_map {
+ public:
+  message_map() = default;
+  ~message_map() = default;
+  std::wstring operator()(DWORD u_msg, WPARAM w_param, LPARAM l_param) const;
+ private:
+  static std::unordered_map<DWORD, std::wstring> messages_;
+};
+
+#endif  // MESSAGE_MAP_H
