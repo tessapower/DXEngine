413969e5fb61db02070e0e3fed3159668654538b
Tessa Power <hello@tessapower.xyz>
2024-11-15 16:51:54 -0500

    Rename exception macros file

---
+++ DXengine/include/exception_macros.h
@@ -0,0 +1,12 @@
+#ifndef EXCEPTION_MACROS_H
+#define EXCEPTION_MACROS_H
+
+#include "app.h"
+
+#define WIDE(x) L##x
+#define W_FILE WIDE(__FILE__)
+
+#define LAST_ERROR app::exception(W_FILE, __LINE__, GetLastError())
+#define HR_ERROR(hr) app::exception(W_FILE, __LINE__, hr)
+
+#endif // EXCEPTION_MACROS_H
