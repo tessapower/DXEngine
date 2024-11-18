4da4344895d325316ae802790f4c0f72ccda8750
Tessa Power <hello@tessapower.xyz>
2024-11-18 10:55:32 -0600

    Implement app exception class

--- DXengine/include/app.h
+++ DXengine/include/app.h
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 
+#include "engine_exception.h"
+
 class app {
  public:
   explicit app(int width = 800, int height = 600, LPCWSTR window_title = L"");
@@ -22,6 +24,26 @@ class app {
   // static callback handle messages
   static LRESULT CALLBACK handle_msg_thunk(HWND h_wnd, UINT u_msg, WPARAM w_param,
                                          LPARAM l_param) noexcept;
+  class exception : public engine_exception {
+  public:
+    exception(const LPCWSTR file, const int line, const HRESULT hr)
+        : engine_exception(file, line), hr_(hr) {
+      type_ = L"App Exception";
+    }
+
+    auto what() const noexcept -> const char* override;
+
+    auto msg() const noexcept -> LPCWSTR override;
+
+    auto error_code() const noexcept -> HRESULT;
+
+    auto error_string() const noexcept -> std::wstring;
+
+    static auto translate_error_code(HRESULT hr) noexcept -> std::wstring;
+
+   private:
+   HRESULT hr_;
+  };
 
  private:
  //----------------------------------------------------------- WindowClass --//
