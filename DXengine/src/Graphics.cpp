66c97865694a5a82b1ecb5fedd2cb7c5f4672811
Tessa Power <hello@tessapower.xyz>
2024-11-20 12:08:01 -0500

    Refactor renderer class to follow naming conventions

--- DXengine/src/Graphics.cpp
+++ DXengine/src/Graphics.cpp
@@ -1,8 +1,9 @@
+#include "stdafx.h"
 #include "Graphics.h"
 
-renderer::renderer(const app& app) : _hWnd(app.h_wnd()) {
+renderer::renderer(const app& app) : h_wnd_(app.h_wnd()) {
   // First create a factory, this lets us create Direct2D resources
-  HRESULT hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &_d2dFactory);
+  HRESULT hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &d2d_factory_);
 
   if (FAILED(hr)) {
     // throw some error
@@ -10,29 +11,33 @@ renderer::renderer(const app& app) : _hWnd(app.h_wnd()) {
 
   // Create Render Target
   // First we need the size of the drawing area
-  GetClientRect(app.h_wnd(), &_clientRect);
+  GetClientRect(app.h_wnd(), &client_rect_);
 
   // Then we create a render target
-  hr = _d2dFactory->CreateHwndRenderTarget(
+  hr = d2d_factory_->CreateHwndRenderTarget(
     D2D1::RenderTargetProperties(), // Rendering options, px format, DPI info, etc
     D2D1::HwndRenderTargetProperties(
       app.h_wnd(),
       D2D1::SizeU(
-        _clientRect.right - _clientRect.left,
-        _clientRect.bottom - _clientRect.top
+        client_rect_.right - client_rect_.left,
+        client_rect_.bottom - client_rect_.top
       )
     ),
-    &_renderTarget
+    &render_target_
   );
 
   if (FAILED(hr)) {
     // throw some error
   }
 
-  _renderTarget->CreateSolidColorBrush(
+  hr = render_target_->CreateSolidColorBrush(
     D2D1::ColorF(D2D1::ColorF::Red),
-    &_redBrush
+    &red_brush_
   );
+
+  if (FAILED(hr)) {
+    // throw some error
+  }
 }
 
 renderer::~renderer() {
@@ -42,18 +47,18 @@ renderer::~renderer() {
 }
 
 void renderer::start_frame() const {
-  _renderTarget_->BeginDraw();
-  _renderTarget_->DrawRectangle(
+  render_target_->BeginDraw();
+  render_target_->DrawRectangle(
     D2D1::RectF(
-      _clientRect.left + 100.0f,
-      _clientRect.top + 100.0f,
-      _clientRect.right - 100.0f,
-      _clientRect.bottom - 100.0f),
-    _redBrush);
+      static_cast<float>(client_rect_.left) + 100.0f,
+      static_cast<float>(client_rect_.top) + 100.0f,
+      static_cast<float>(client_rect_.right) - 100.0f,
+      static_cast<float>(client_rect_.bottom) - 100.0f),
+    red_brush_);
 }
 
-void Graphics::endFrame() {
-  HRESULT hr = _renderTarget->EndDraw();
+void renderer::end_frame() const {
+  HRESULT hr = render_target_->EndDraw();
   
   if (FAILED(hr)) {
     // throw an error here or handle needing to recreate resources
