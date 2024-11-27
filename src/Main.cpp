1f81754ae9fb7c75fbcb36b6d882ff48387e9d31
Tessa Power <hello@tessapower.xyz>
2024-11-27 16:46:50 -0600

    Remove unnecessary ImGui demo code

--- src/Main.cpp
+++ src/Main.cpp
@@ -23,8 +23,7 @@ auto cleanup_render_target() -> void;
 auto WINAPI wnd_proc(HWND h_wnd, UINT msg, WPARAM w_param,
                      LPARAM l_param) -> LRESULT;
 
-// Main code
-int main(int, char**) {
+auto main(int, char**) -> int {
   // Create application window
   ImGui_ImplWin32_EnableDpiAwareness();
   const WNDCLASSEXW wc = {sizeof(wc),
@@ -73,8 +72,6 @@ int main(int, char**) {
   ImGui_ImplDX11_Init(g_pd3d_device, g_pd3d_device_context);
 
   // Our state
-  bool show_demo_window = true;
-  bool show_another_window = false;
   auto clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
   // Main loop
@@ -84,9 +81,9 @@ int main(int, char**) {
     // See the WndProc() function below for our to dispatch events to the Win32
     // backend.
     MSG msg;
-    while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
+    while (PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
       TranslateMessage(&msg);
-      ::DispatchMessage(&msg);
+      DispatchMessage(&msg);
       if (msg.message == WM_QUIT) done = true;
     }
     if (done) break;
@@ -113,56 +110,23 @@ int main(int, char**) {
     ImGui_ImplWin32_NewFrame();
     ImGui::NewFrame();
 
-    // 1. Show the big demo window (Most of the sample code is in
-    // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
-    // ImGui!).
-    if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
-
-    // 2. Show a simple window that we create ourselves. We use a "Begin/End"
-    // pair to create a named window.
     {
-      static float f = 0.0f;
-      static int counter = 0;
+      ImGui::Begin("Shaders");
+      ImGui::Text(
+          "This window will display a list of shaders that \nare dynamically "
+          "loaded and applied to the scene.");
 
-      ImGui::Begin("Hello, world!");  // Create a window called "Hello, world!"
-                                      // and append into it.
+      ImGui::Spacing();
 
-      ImGui::Text("This is some useful text.");  // Display some text (you can
-                                                 // use a format strings too)
-      ImGui::Checkbox(
-          "Demo Window",
-          &show_demo_window);  // Edit bools storing our window open/close state
-      ImGui::Checkbox("Another Window", &show_another_window);
-
-      ImGui::SliderFloat(
-          "float", &f, 0.0f,
-          1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
       ImGui::ColorEdit3(
           "clear color",
           reinterpret_cast<float*>(
               &clear_color));  // Edit 3 floats representing a color
 
-      if (ImGui::Button(
-              "Button"))  // Buttons return true when clicked (most widgets
-                          // return true when edited/activated)
-        counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
+      ImGui::Spacing();
 
       ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
                   1000.0f / io.Framerate, io.Framerate);
-      ImGui::End();
-    }
-
-    // 3. Show another simple window.
-    if (show_another_window) {
-      ImGui::Begin(
-          "Another Window",
-          &show_another_window);  // Pass a pointer to our bool variable (the
-                                  // window will have a closing button that will
-                                  // clear the bool when clicked)
-      ImGui::Text("Hello from another window!");
-      if (ImGui::Button("Close Me")) show_another_window = false;
       ImGui::End();
     }
 
@@ -269,9 +233,8 @@ auto cleanup_render_target() -> void {
 }
 
 // Forward declare message handler from imgui_impl_win32.cpp
-extern auto ImGui_ImplWin32_WndProcHandler(HWND h_wnd, UINT msg,
-                                               WPARAM w_param,
-                                               LPARAM l_param) -> LRESULT;
+extern auto ImGui_ImplWin32_WndProcHandler(HWND h_wnd, UINT msg, WPARAM w_param,
+                                           LPARAM l_param) -> LRESULT;
 
 // Win32 message handler
 // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if
@@ -284,8 +247,7 @@ extern auto ImGui_ImplWin32_WndProcHandler(HWND h_wnd, UINT msg,
 // your application based on those two flags.
 auto WINAPI wnd_proc(const HWND h_wnd, const UINT msg, const WPARAM w_param,
                      const LPARAM l_param) -> LRESULT {
-  if (ImGui_ImplWin32_WndProcHandler(h_wnd, msg, w_param, l_param))
-    return true;
+  if (ImGui_ImplWin32_WndProcHandler(h_wnd, msg, w_param, l_param)) return true;
 
   switch (msg) {
     case WM_SIZE:
