27d2bc335e6cbaaa62ec7b8630f4f1f333ba0858
Tessa Power <hello@tessapower.xyz>
2024-12-10 14:51:14 -0600

    Create dedicated gui class

---
+++ include/gui.h
@@ -0,0 +1,29 @@
+﻿#ifndef GUI_H
+#define GUI_H
+
+#include "stdafx.h"
+#include <imgui.h>
+
+class gui {
+  int width_;
+  int height_;
+  ImGuiIO* io_;
+  ImVec4 clear_color_ = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+ public:
+  explicit gui(int viewport_width = 0, int viewport_height = 0);
+  gui(const gui&) = delete;
+  gui(const gui&&) = delete;
+  ~gui();
+
+  auto init() -> void;
+
+  auto set_viewport(int width, int height) noexcept -> void;
+
+  auto update() const noexcept -> void;
+
+  auto render() const noexcept -> void;
+
+};
+
+#endif  // GUI_H
