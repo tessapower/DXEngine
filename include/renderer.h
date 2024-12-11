b997c53354daee69aac685c4dbf26cd1b6523578
Tessa Power <hello@tessapower.xyz>
2024-12-11 12:03:13 -0500

    Convert renderer data and functions into class

--- include/renderer.h
+++ include/renderer.h
@@ -1,20 +1,41 @@
 #ifndef RENDERER_H
 #define RENDERER_H
+#include "stdafx.h"
 #include <d3d11.h>
 
-// Data
-inline ID3D11Device* g_p_d3d_device = nullptr;
-inline ID3D11DeviceContext* g_p_d3d_device_context = nullptr;
-inline IDXGISwapChain* g_p_swap_chain = nullptr;
-inline bool g_swap_chain_occluded = false;
-inline UINT g_resize_width = 0, g_resize_height = 0;
-inline ID3D11RenderTargetView* g_main_render_target_view = nullptr;
+class renderer {
+ public:
+  ID3D11Device* p_device = nullptr;
+  IDXGISwapChain* p_swap_chain = nullptr;
+  ID3D11DeviceContext* p_device_context = nullptr;
 
-auto create_device_d3d(HWND h_wnd) -> bool;
-auto cleanup_device_d3d() -> void;
-auto create_render_target() -> void;
-auto cleanup_render_target() -> void;
-auto init_backends(HWND h_wnd) -> void;
-auto shut_down() -> void;
+  bool swap_chain_occluded = false;
+  UINT resize_width = 0;
+  UINT resize_height = 0;
+  ID3D11RenderTargetView* main_render_target_view = nullptr;
+
+  renderer() = default;
+  ~renderer() = default;
+  renderer(const renderer&) = delete;
+  renderer& operator=(const renderer&) = delete;
+  renderer(const renderer&&) = delete;
+  renderer& operator=(const renderer&&) = delete;
+
+  auto clear_back_buffer(const float clear_color[4]) const noexcept-> void;
+
+  auto create_device_d3d(HWND h_wnd) -> bool;
+
+  auto init_backends(HWND h_wnd) -> void;
+
+  auto create_render_target() -> void;
+
+  // TODO: use these in dtor?
+
+  auto cleanup_device_d3d() -> void;
+
+  auto cleanup_render_target() -> void;
+
+  auto shut_down() -> void;
+};
 
 #endif // RENDERER_H
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,90 +1,119 @@
-#include "stdafx.h"
 #include "renderer.h"
 
 #include <imgui_impl_dx11.h>
 
 #include "imgui_impl_win32.h"
+#include "stdafx.h"
 
-auto init_backends(const HWND h_wnd) -> void {
-  ImGui_ImplWin32_Init(h_wnd);
-  ImGui_ImplDX11_Init(g_p_d3d_device, g_p_d3d_device_context);
+auto renderer::clear_back_buffer(const float clear_color[4]) const noexcept -> void {
+  p_device_context->OMSetRenderTargets(1, &main_render_target_view,
+                                         nullptr);
+  p_device_context->ClearRenderTargetView(main_render_target_view, clear_color);
 }
 
-auto create_device_d3d(const HWND h_wnd) -> bool {
-  // Setup swap chain
+
+auto renderer::create_device_d3d(const HWND h_wnd) -> bool {
+  // Set up swap chain descriptor
   DXGI_SWAP_CHAIN_DESC sd;
-  ZeroMemory(&sd, sizeof(sd));
-  sd.BufferCount = 2;
+  ZeroMemory(&sd, sizeof(sd));  // Ensure that the memory is zeroed out
+  sd.BufferCount = 2;           // Number of back buffers
   sd.BufferDesc.Width = 0;
   sd.BufferDesc.Height = 0;
-  sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-  sd.BufferDesc.RefreshRate.Numerator = 60;
-  sd.BufferDesc.RefreshRate.Denominator = 1;
+  sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // Pixel channel layout
+  sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+  sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+  sd.BufferDesc.RefreshRate.Numerator = 0;         // Use existing refresh rate
+  sd.BufferDesc.RefreshRate.Denominator = 0;       // Use existing refresh rate
   sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
   sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
   sd.OutputWindow = h_wnd;
-  sd.SampleDesc.Count = 1;
-  sd.SampleDesc.Quality = 0;
+  sd.SampleDesc.Count = 1;      // Antialiasing i.e. nothing for now
+  sd.SampleDesc.Quality = 0;    // Antialiasing i.e. nothing for now
   sd.Windowed = TRUE;
-  sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+  sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;  // Effect used for presentation
 
   constexpr UINT create_device_flags = 0;
   // createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+
   D3D_FEATURE_LEVEL feature_level;
   constexpr D3D_FEATURE_LEVEL feature_level_array[2] = {
       D3D_FEATURE_LEVEL_11_0,
       D3D_FEATURE_LEVEL_10_0,
   };
+
   HRESULT res = D3D11CreateDeviceAndSwapChain(
-      nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, create_device_flags,
-      feature_level_array, 2, D3D11_SDK_VERSION, &sd, &g_p_swap_chain,
-      &g_p_d3d_device, &feature_level, &g_p_d3d_device_context);
-  if (res == DXGI_ERROR_UNSUPPORTED)  // Try high-performance WARP software
-                                      // driver if hardware is not available.
+      nullptr,  // Pointer to the video adapter to use when creating a device.
+      D3D_DRIVER_TYPE_HARDWARE,  // Driver Type
+      nullptr, // Handle to software driver binary
+      create_device_flags, //
+      feature_level_array,       // Feature Levels
+      2, // Feature Levels
+      D3D11_SDK_VERSION, // SDK Version
+      &sd,
+      &p_swap_chain,
+      &p_device,
+      &feature_level,  // Returns the feature level of the device created
+      &p_device_context);
+
+  // Try high-performance WARP software driver if hardware is not available.
+  if (res == DXGI_ERROR_UNSUPPORTED) {
     res = D3D11CreateDeviceAndSwapChain(
         nullptr, D3D_DRIVER_TYPE_WARP, nullptr, create_device_flags,
-        feature_level_array, 2, D3D11_SDK_VERSION, &sd, &g_p_swap_chain,
-        &g_p_d3d_device, &feature_level, &g_p_d3d_device_context);
+        feature_level_array, 2, D3D11_SDK_VERSION, &sd, &p_swap_chain,
+        &p_device, &feature_level, &p_device_context);
+  }
+
   if (res != S_OK) return false;
 
   create_render_target();
+
   return true;
 }
 
-auto cleanup_device_d3d() -> void {
+auto renderer::init_backends(const HWND h_wnd) const -> void {
+  ImGui_ImplWin32_Init(h_wnd);
+  ImGui_ImplDX11_Init(p_device, p_device_context);
+}
+
+auto renderer::cleanup_device_d3d() -> void {
   cleanup_render_target();
-  if (g_p_swap_chain) {
-    g_p_swap_chain->Release();
-    g_p_swap_chain = nullptr;
+  if (p_swap_chain) {
+    p_swap_chain->Release();
+    p_swap_chain = nullptr;
   }
-  if (g_p_d3d_device_context) {
-    g_p_d3d_device_context->Release();
-    g_p_d3d_device_context = nullptr;
+  if (p_device_context) {
+    p_device_context->Release();
+    p_device_context = nullptr;
   }
-  if (g_p_d3d_device) {
-    g_p_d3d_device->Release();
-    g_p_d3d_device = nullptr;
+  if (p_device) {
+    p_device->Release();
+    p_device = nullptr;
   }
 }
 
-auto create_render_target() -> void {
-  ID3D11Texture2D* p_back_buffer;
+auto renderer::create_render_target() -> void {
+  // Gain access to texture subresource in swap chain (back buffer)
+  ID3D11Resource* p_back_buffer;
 
   // TODO: handle unsuccessful render target creation
-  g_p_swap_chain->GetBuffer(0, IID_PPV_ARGS(&p_back_buffer));
-  g_p_d3d_device->CreateRenderTargetView(p_back_buffer, nullptr,
-                                        &g_main_render_target_view);
+  p_swap_chain->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&p_back_buffer));
+  p_device->CreateRenderTargetView(
+    p_back_buffer,
+    nullptr,
+    &main_render_target_view
+  );
+
   p_back_buffer->Release();
 }
 
-auto cleanup_render_target() -> void {
-  if (g_main_render_target_view) {
-    g_main_render_target_view->Release();
-    g_main_render_target_view = nullptr;
+auto renderer::cleanup_render_target() -> void {
+  if (main_render_target_view) {
+    main_render_target_view->Release();
+    main_render_target_view = nullptr;
   }
 }
 
-auto shut_down() -> void {
+auto renderer::shut_down() -> void {
   ImGui_ImplDX11_Shutdown();
   ImGui_ImplWin32_Shutdown();
 
