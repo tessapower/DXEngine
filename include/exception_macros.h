27ef4f846486dde62069f5e0b4064ec548a5072b
Tessa Power <hello@tessapower.xyz>
2024-12-12 10:14:34 -0600

    Create macros for throwing renderer exceptions

--- include/exception_macros.h
+++ include/exception_macros.h
@@ -7,6 +7,13 @@
 #define W_FILE WIDE(__FILE__)
 
 #define LAST_ERROR app::exception(W_FILE, __LINE__, GetLastError())
+
+// HRESULT hr should exist in the local scope for these macros to work
 #define HR_ERROR(hr) app::exception(W_FILE, __LINE__, hr)
+
+#define RNDR_EXCEPT(hr) renderer::hr_exception(W_FILE, __LINE__, (hr))
+#define RNDR_THROW(hr_call) if (FAILED(hr = (hr_call))) throw RNDR_EXCEPT(hr)
+#define RNDR_DEVICE_REMOVED_EXCEPT(hr) \
+  renderer::device_remove_exception(W_FILE, __LINE__, (hr))
 
 #endif // EXCEPTION_MACROS_H
