1e9f304c17ee734881c4f22838cba2609a246ab1
Tessa Power <hello@tessapower.xyz>
2024-12-13 14:14:10 -0600

    Add error handling in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,37 @@
 #include "stdafx.h"
 #include "app.h"
 #include "imgui.h"
-#include "imgui_impl_dx11.h"
-#include "imgui_impl_win32.h"
 
 auto main(int, char**) -> int {
-  // Create a window
-  app app(1280, 800, L"DX Engine");
+  try {
+    // Create a window
+    app app(1280, 800, L"DX Engine");
 
-  // Initialize the GUI/Window
-  app.init_gui();
+    // Initialize the GUI/Window
+    app.init_gui();
 
-  // Main loop
-  bool done = false;
-  while (!done) {
-    app.update(done);
-    app.render();
+    // Main loop
+    bool done = false;
+    while (!done) {
+      app.update(done);
+      app.render();
+    }
+  } catch (const engine_exception& e) {
+
+    MessageBoxW(nullptr, e.msg(), e.type(), MB_OK | MB_ICONEXCLAMATION);
+
+    return EXIT_FAILURE;
+  } catch (const std::exception& e) {
+    OutputDebugStringA(e.what());
+    MessageBoxA(nullptr, e.what(), "Standard Exception",
+               MB_OK | MB_ICONEXCLAMATION);
+
+    return EXIT_FAILURE;
+  } catch (...) {
+    MessageBoxW(nullptr, L"No details available", L"Unknown Exception",
+               MB_OK | MB_ICONEXCLAMATION);
+
+    return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
