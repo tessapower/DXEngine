c04770dfe1d9aec2929ccf0be6454610d616e3b8
Tessa Power <hello@tessapower.xyz>
2024-12-18 09:49:23 -0500

    Create macros for shader source and compiled files

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -8,6 +8,10 @@
 #include "exception_macros.h"
 #include "imgui_impl_win32.h"
 
+#include <d3d11.h>
+#include <iostream>
+#include <filesystem>
+
 //----------------------------------------------------------- hr_exception --//
 
 renderer::hr_exception::hr_exception(
@@ -157,6 +161,7 @@ auto renderer::shut_down() -> void {
 auto renderer::test_draw() -> void {
   namespace wrl = Microsoft::WRL;
 
+  //------------------------------------------------------------- Vertices --//
   struct vertex {
     float x;
     float y;
@@ -171,7 +176,8 @@ auto renderer::test_draw() -> void {
   bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
   bd.CPUAccessFlags = 0u;
   bd.MiscFlags = 0u;
-  bd.StructureByteStride = sizeof(vertex);
+  bd.ByteWidth = sizeof(vertex);
+  bd.StructureByteStride = 0;
 
   HRESULT hr;
   D3D11_SUBRESOURCE_DATA sd = {};
@@ -181,18 +187,145 @@ auto renderer::test_draw() -> void {
   // Bind vertex buffer to pipeline
   constexpr UINT stride = sizeof(vertex);
   constexpr UINT offset = 0u;
-  p_device_context->IASetVertexBuffers(0u, 1, &p_vertex_buffer, &stride, &offset);
+  p_device_context->IASetVertexBuffers(0u, 1, p_vertex_buffer.GetAddressOf(), &stride, &offset);
 
-  // Create vertex shader
-  wrl::ComPtr<ID3D11VertexShader> p_vertex_shader;
+  //--------------------------------------------------------- Pixel Shader --//
+  // Create pixel shader
+  wrl::ComPtr<ID3D11PixelShader> p_pixel_shader;
   wrl::ComPtr<ID3DBlob> p_blob;
-  RNDR_THROW(D3DReadFileToBlob(L"${SHADER_DIR}/vertex_shader.cso", &p_blob));
-  RNDR_THROW(p_device->CreateVertexShader(p_blob->GetBufferPointer(),
-                               p_blob->GetBufferSize(), nullptr,
-                               &p_vertex_shader));
+  Microsoft::WRL::ComPtr<ID3DBlob> error_blob;
+
+  auto path = std::filesystem::path(SHADER_SRC_DIR) / "pixel_shader.ps.hlsl";
+  std::wstring wide_path = path.wstring();
+
+  if (!exists(path)) {
+    std::cerr << "Error: Pixel shader file not found.\n";
+
+    return;
+  }
+
+  // Compile pixel shader
+  hr = D3DCompileFromFile(
+    wide_path.c_str(), // HLSL file
+    nullptr,           // Macros
+    nullptr,           // Include handler
+    "main",            // Entry point
+    "ps_5_0",          // Target shader model
+    D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // Compile flags
+    0,                                               // Effect flags
+    p_blob.GetAddressOf(),    // Compiled shader output
+    error_blob.GetAddressOf() // Error messages
+  );
+
+
+  // TODO: Add macro that uses error_blob messages in case of failure
+  if (FAILED(hr)) {
+    std::cerr << "Error: Failed to compile pixel shader.\n";
+
+    if (error_blob) {
+      std::cerr << "Shader Compilation Error: "
+                << static_cast<char*>(error_blob->GetBufferPointer()) << "\n";
+    }
+
+    return;
+  }
+
+  // Create the pixel shader
+  RNDR_THROW(
+    p_device->CreatePixelShader(
+      p_blob->GetBufferPointer(),
+      p_blob->GetBufferSize(),
+      nullptr,
+      &p_pixel_shader
+    )
+  );
+
+  // Bind pixel shader to pipeline
+  p_device_context->PSSetShader(p_pixel_shader.Get(), nullptr, 0u);
+
+  //-------------------------------------------------------- Vertex Shader --//
+  wrl::ComPtr<ID3D11VertexShader> p_vertex_shader;
+
+  path =
+      std::filesystem::path(SHADER_SRC_DIR) / "vertex_shader.vs.hlsl";
+  wide_path = path.wstring();
+
+  if (!exists(path)) {
+    std::cerr << "Error: Vertex shader file not found.\n";
+
+    return;
+  }
+
+  // Compile vertex shader
+  hr = D3DCompileFromFile(
+      wide_path.c_str(),                                // HLSL file
+      nullptr,                                          // Macros
+      nullptr,                                          // Include handler
+      "main",                                           // Entry point
+      "vs_5_0",                                         // Target shader model
+      D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,  // Compile flags
+      0,                                                // Effect flags
+      p_blob.GetAddressOf(),     // Compiled shader output
+      error_blob.GetAddressOf()  // Error messages
+  );
+
+  // TODO: Add macro that uses error_blob messages in case of failure
+  if (FAILED(hr)) {
+    std::cerr << "Error: Failed to compile vertex shader.\n";
+
+    if (error_blob) {
+      std::cerr << "Shader Compilation Error: "
+                << static_cast<char*>(error_blob->GetBufferPointer()) << "\n";
+    }
+
+    return;
+  }
+
+  // Create the vertex shader
+  RNDR_THROW(
+    p_device->CreateVertexShader(
+      p_blob->GetBufferPointer(),
+      p_blob->GetBufferSize(),
+      nullptr,
+      &p_vertex_shader
+    )
+  );
 
   // Bind vertex shader to pipeline
   p_device_context->VSSetShader(p_vertex_shader.Get(), nullptr, 0u);
+  
+  //--------------------------------------------------------- Input Layout --//
+  // Input (vertex) layout (2D position only)
+  wrl::ComPtr<ID3D11InputLayout> p_input_layout;
+  constexpr D3D11_INPUT_ELEMENT_DESC ied[] = {
+      {"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0,
+       D3D11_INPUT_PER_VERTEX_DATA, 0}
+  };
 
+  // Create input layout
+  RNDR_THROW(p_device->CreateInputLayout(
+      ied, std::size(ied), p_blob->GetBufferPointer(), p_blob->GetBufferSize(),
+      &p_input_layout));
+
+  // Bind input layout to pipeline
+  p_device_context->IASetInputLayout(p_input_layout.Get());
+
+  // Bind the render target
+  p_device_context->OMSetRenderTargets(1u, &p_render_target_view, nullptr);
+
+  // Set primitive topology to triangle list
+  p_device_context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+  // Configure view port
+  D3D11_VIEWPORT vp;
+  vp.Width = 1280;
+  vp.Height = 800;
+  vp.MinDepth = 0;
+  vp.MaxDepth = 1;
+  vp.TopLeftX = 0;
+  vp.TopLeftY = 0;
+  p_device_context->RSSetViewports(1u, &vp);
+
+  // Draw the thing
   p_device_context->Draw(std::size(vertices), 0u);
 }
