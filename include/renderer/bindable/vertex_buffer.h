236935b5922396a890afa83ff344b40132927d2d
Tessa Power <hello@tessapower.xyz>
2025-01-14 15:30:52 -0600

    Create vertex_buffer class

---
+++ include/renderer/bindable/vertex_buffer.h
@@ -0,0 +1,36 @@
+#ifndef VERTEX_BUFFER_H
+#define VERTEX_BUFFER_H
+
+#include "stdafx.h"
+#include "buffer.h"
+
+#include <wrl.h>
+
+class vertex_buffer : public buffer {
+ public:
+  template <typename T>
+  vertex_buffer(renderer& rndr, std::vector<T>& vertices)
+    : stride_(sizeof(T)) {
+    buffer_descriptor_.Usage = D3D11_USAGE_DEFAULT;
+    buffer_descriptor_.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    buffer_descriptor_.CPUAccessFlags = 0u;
+    buffer_descriptor_.MiscFlags = 0u;
+    buffer_descriptor_.ByteWidth = static_cast<UINT>(sizeof(T) * vertices.size());
+    buffer_descriptor_.StructureByteStride = sizeof(T);
+
+    subresource_data_.pSysMem = vertices.data();
+
+    HRESULT hr;
+    RENDER_THROW_ON_FAIL(device(rndr)->CreateBuffer(&buffer_descriptor_, &subresource_data_, &buffer_));
+  }
+
+  virtual auto bind(renderer& rndr) noexcept -> void override {
+    constexpr UINT offset = 0u;
+    context(rndr)->IASetVertexBuffers(0u, 1u, buffer_.GetAddressOf(), &stride_, &offset);
+  }
+
+ protected:
+  UINT stride_;
+};
+
+#endif // VERTEX_BUFFER_H
--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -224,23 +224,6 @@ auto renderer::test_draw() -> void {
   // Vertex Buffer
   wrl::ComPtr<ID3D11Buffer> p_vertex_buffer;
 
-  D3D11_BUFFER_DESC bd = {};
-  bd.Usage = D3D11_USAGE_DEFAULT;
-  bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-  bd.CPUAccessFlags = 0u;
-  bd.MiscFlags = 0u;
-  bd.ByteWidth = sizeof(vertices);
-  bd.StructureByteStride = sizeof(vertex);
-
-  D3D11_SUBRESOURCE_DATA sd = {};
-  sd.pSysMem = vertices;
-  RENDER_THROW_ON_FAIL(p_device_->CreateBuffer(&bd, &sd, &p_vertex_buffer));
-
-  // Bind vertex buffer to pipeline
-  constexpr UINT stride = sizeof(vertex);
-  constexpr UINT offset = 0u;
-  p_device_context_->IASetVertexBuffers(0u, 1u, p_vertex_buffer.GetAddressOf(),
-                                        &stride, &offset);
 
   //--------------------------------------------------------- Index Buffer --//
 
