9bd269608a63915ba81bb27a9a80f12cc74575e9
Tessa Power <hello@tessapower.xyz>
2025-01-16 10:39:41 -0600

    Reformat bindable header and add comments

--- include/renderer/bindable/bindable.h
+++ include/renderer/bindable/bindable.h
@@ -9,16 +9,28 @@ class bindable {
   virtual ~bindable() = default;
 
  protected:
+  // The following are helper methods to enable bindable child classes to
+  // have access to private members of renderer without needing to make every
+  // class a friend of renderer.
+
+  /** @brief Returns a pointer to the given renderer's device context.
+  */
   static auto context(renderer& rndr) noexcept -> ID3D11DeviceContext* {
     return rndr.p_device_context_.Get();
   }
 
+  /** @brief Returns a reference to the given renderer's dxgi_info_manager.
+   * Used to pull the dxgi_info_manager into scope for specific macros that
+   * require its presence.
+   */
   static dxgi_info_manager& info_manager(renderer& rndr) noexcept {
     return rndr.dxgi_info_mgr_;
   }
 
+  /** @brief Returns a pointer to the given renderer's device.
+   */
   static ID3D11Device* device(renderer& rndr) noexcept {
-      return rndr.p_device_.Get();
+    return rndr.p_device_.Get();
   }
 };
 
