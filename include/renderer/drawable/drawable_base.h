009510dad55662a026007bf9f3443c0cafd68a10
Tessa Power <hello@tessapower.xyz>
2025-01-24 16:48:51 -0600

    Create header to include all required headers for drawable objects

---
+++ include/renderer/drawable/drawable_base.h
@@ -0,0 +1,15 @@
+#ifndef DRAWABLE_BASE_H
+#define DRAWABLE_BASE_H
+
+// Includes the bindable headers required to create a drawable object
+
+#include "renderer/bindable/const_buffer.h"
+#include "renderer/bindable/index_buffer.h"
+#include "renderer/bindable/input_layout.h"
+#include "renderer/bindable/pixel_shader.h"
+#include "renderer/bindable/topology.h"
+#include "renderer/bindable/transform_buffer.h"
+#include "renderer/bindable/vertex_buffer.h"
+#include "renderer/bindable/vertex_shader.h"
+
+#endif // DRAWABLE_BASE_H
